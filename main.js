d6a9ecdbc54e7e566c1a4b5c41f07f0d21b07d0c
jesus1554 <jesus.flores.esp@gmail.com>
2020-05-14 11:47:33 -0500

    Comenzando JS del menu

---
+++ main.js
@@ -0,0 +1,9 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.links');
+
+    burger.addEventListener('click', () => {
+        
+    })
+
+};
